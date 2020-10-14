de4e65805ab090f00c2939e3797308fdaa992722
Beat <balvarezlacasa@hotmail.com>
2020-10-14 16:55:53 +0200

    cambios host puerto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const http = require("./app");
+const host = "localhost" || process.env.HOST;
 const port = process.env.PORT || 5000;
-http.listen(port, () => {
-  console.log("Servidor en puerto 5000");
+http.listen(port, host, () => {
+  console.log("Servidor en ip: " + host + " puerto:" + port);
 });
