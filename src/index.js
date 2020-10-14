8e0a72c6496ec6d8768690e40b5d56b92b3c39c0
Beat <balvarezlacasa@hotmail.com>
2020-10-14 17:04:35 +0200

    server express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-const http = require("./app");
+//const http = require("./app");
+// const host = "localhost" || process.env.HOST;
+// const port = process.env.PORT || 5000;
+// http.listen(port, host, () => {
+//   console.log("Servidor en ip: " + host + " puerto:" + port);
+// });
+
+const express = require("express");
+const app = express();
 const host = "localhost" || process.env.HOST;
 const port = process.env.PORT || 5000;
-http.listen(port, host, () => {
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
   console.log("Servidor en ip: " + host + " puerto:" + port);
 });
